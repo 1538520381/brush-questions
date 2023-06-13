4c736419a1e5c3b6d4f9794309fc443f82aea236
1538520381 <1538520381@qq.com>
2023-06-13 20:40:11 +0800

    基本框架搭建

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export const constantRoutes = [
+    {
+        path: '/',
+        redirect: '/index'
+    },
+    {
+        path: '/index',
+        name: 'index',
+        component: () => import('@/views')
+    },
+]
+
+const createRouter = () => new VueRouter({
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+    mode: 'history'
+})
+
+const router = createRouter()
+
+export default router
